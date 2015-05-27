d9755713cb55369b8feec7d1c49bb2ba4b6efac0
superdyoll <superdyoll@gmail.com>
2015-05-27 22:08:32 +0100

    First commit

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    	<meta name="description" content=""/>
+    	<meta name="author" content=""/>
+    	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (C) 2013 peredur.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+$page = 'home';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    	<title>Forest Evasion 2016</title>
+		<?php include 'includes/head.php';?>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+<body>
+	<?php include 'includes/navbar.php'; ?>
+	<div class="jumbotron jumboback">
+    	<!--<div class="background-image-overlay">-->
+        	<div class="container">
+      			<div class="row"> 
+            		<div class="col-md-12">
+               			<h1>Forest Evasion 2016</h1>
+            			<br>
+            			<br>
+            			<a class="btn btn-large btn-info" style="text-shadow: white 0em 0em 0em; padding-left:15px; padding-right:15px;">Sign up now!</a>
+                	</div>
+            	</div>
+        	</div>
+        <!--</div>-->
+     </div>
+     <div class="container">
+      	<div class="row">
+        <div class="col-md-8">
+          <h2>The Challenge</h2>
+          <p>We are passionate about QR codes and using them to create an 
+interactive
+            experience to allow you to follow routes created by others and 
+experience
+            new content. You can also create your own routes</p>
+          <a class="btn btn-info" href="aboutus.php">About Us</a>
+        </div>
+        <div class="col-md-4">
+          <img src="images/EvasionGroup.jpg" class="img-responsive"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-11">
+          <hr>
+        </div>
+      </div>
+    </div>        
+    </body>
+</html>
