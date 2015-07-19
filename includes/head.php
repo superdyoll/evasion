8f1f99dcbed3dc519ff1e6647e47bf4fe4160680
superdyoll <superdyoll@gmail.com>
2015-07-19 23:54:35 +0100

    new css theme

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     	<meta name="description" content=""/>
     	<meta name="author" content=""/>
-    	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    	<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <script type="text/javascript" src="js/jquery.min.js"></script>
 		<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/style.css">
