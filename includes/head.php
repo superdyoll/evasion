06044a5fc3a9254346193e4e99c20d52e24b3dc5
superdyoll <superdyoll@gmail.com>
2016-08-11 19:01:30 -0400

    Updated to temporary site

--- includes/head.php
+++ includes/head.php
@@ -8,12 +8,33 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/navbar.css">
 <script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	
-	ga('create', 'UA-54103435-3', 'auto');
-	ga('send', 'pageview');
-	
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-54103435-3', 'auto');
+    ga('send', 'pageview');
+    
+</script>
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "SocialEvent",
+  "name": "Forest Evasion",
+  "startDate": "2017-04-21T18:00",
+  "endDate":"2017-04-23T18:00",
+  "location": {
+    "@type": "Place",
+    "name": "Two Mile Bottom Campsite",
+    "address": "Two Mile Bottom Camp Site,Mundford Road,Thetford, Norfolk,IP24 1LZ"
+  },
+  "description": "Forest Evasion is an Annual Navigation Competition where competitors must use their skills to evade capture",
+  "image": "http://forestevasion.co.uk/images/FElogo2016_NoText.png",
+  "url":"http://forestevasion.co.uk",
+  "organizer": {
+    "@type":"Organization",
+    "name":"Forest Evasion"
+   }
+}
 </script>
--- index.php
+++ index.php
@@ -20,8 +20,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Forest Evasion 2016</title>
+        <title>Forest Evasion 2017</title>
         <?php include 'includes/head.php';?>
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
         <script language="javascript">
             $('.carousel').carousel()
         </script>
@@ -42,48 +43,46 @@
         <link rel="stylesheet" href="css/font-awesome.min.css">
     </head>
     <body data-spy="scroll" data-target="#navbar-top">
-        <?php include 'includes/navbar.php'; ?>
-        <div id="home" class="jumbotron jumboback inset-shadow-bar">
+        <div class="title">
             <div class="container">
-                <div class="row"> 
-                    <div class="col-md-12">
-                        <h1>Forest Evasion 2016</h1>
-                        <h2>23rd - 24th April</h2>
-                        <br>
-                        <a href="#signup" class="btn btn-large btn-info" style="text-shadow: white 0em 0em 0em; padding-left:15px; padding-right:15px;">Sign up now!</a>
+                <div class="row">
+                    <div class="col-md-2">
+                        <img class="logo" src="images/FElogo2016_NoText.png" />
+                    </div>
+                    <div class="col-md-8">
+                        <h1>FOREST EVASION 2017</h1>
+                        <h2>THETFORD FOREST</h2>
+                        <h3>21st - 23rd April 2017</h3>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="icons">
+                            <a href="https://www.facebook.com/ForestEvasion/"><i class="fa fa-facebook fa-lg icon"></i></a>
+                            <a href="https://twitter.com/ForestEvasion"><i class="fa fa-twitter fa-lg icon"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="container">
-            <div id="about" class="row padded-row-top">
+            <div class="row">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/9NYaBlqgFtg" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="row padded-row-top">
+                <div class="col-md-4">
+                    <img src="images/Group.jpg" class="img-responsive shadow-image"/>
+                </div>
                 <div class="col-md-8">
-                    <h3>What is this?</h3>
-                    <p>Forest Evasion is our annual competition with the aim of meeting and interacting with other like-minded clubs. 2016 will be the 4th year the competition has run. This page is designed to give an overview of what to expect from Evasion, full rules will be sent to participants ahead of the competition.</p>
+                    <h3>What is Forest Evasion?</h3>
+                    <p>Forest Evasion is our annual competition with the aim of meeting and interacting with other like-minded clubs. 2017 will be the 5th year the competition has run. This page is designed to give an overview of what to expect from Evasion, full rules will be sent to participants ahead of the competition.</p>
                     <h3>The Competition</h3>
                     <p>Evasion is a 24 hour competition designed to appeal to outdoor societies with teams of <b>'Evaders'</b> (4-7 people) or <b>'Catchers'</b> (working individually or as a team). The Evaders' task is to navigate between a number of given checkpoints while the Catchers attempt to find them. Each time a catching team catches an evading team they gain points and the Evaders lose a life. Ultimately the teams with the most lives or the most points after 24 hours are the winners.</p>
                 </div>
-                <div class="col-md-4">
-                    <img src="images/EvasionGroup.jpg" class="img-responsive shadow-image"/>
-                </div>
             </div>
             <div class="row">
-                <div class="col-md-4 hidden-xs hidden-sm">
-                    <img src="images/EvasionCompetitors.jpg" class="img-responsive shadow-image"/>
-                </div>
-                <div class="col-md-8">
-                    <h3>When is it?</h3>
-                    <p>In 2016 the competition will take place on the weekend of 22nd-24th April.</p>
-                    <h3>Where?</h3>
-                    <p>This year we're really excited to be going back to the Forest of Dean, a beautiful location which presents a great competition area. The 2015 competition was here too and it was the best so far!</p>
-                    <h3>How much does it cost?</h3>
-                    <p>The cost for this years competition will be £7 per person which includes overnight camping at Botany Bay Campsite</p>
-                    <!--    <h3>How do I sign up?</h3>
-                    Sign up for 2016 is now live!!! Simply fill out the form and email it back to <a href="mailto:info@forestevasion.co.uk">info@forestevasion.co.uk</a> and you'll be ready to go. --> 
-                    <br />
-                    <center>
-                        <a href="#signup" class="btn btn-success btn-lg text-center">Sign up now!</a>
-                    </center> 
+                <div class="col-md-4 col-md-offset-4">
+                    <a class="btn btn-success btn-register btn-shadow" href="https://docs.google.com/forms/d/e/1FAIpQLSdqg6fdWs-YK14-6iWt_tOHch43FaBfFpRh_S3W7Gq7LlIqZA/viewform?usp=send_form">Register your interest now >></a>
                 </div>
             </div>
             <div class="row">
@@ -143,128 +142,39 @@
                 </div>
             </div>
             
-            <div class="row" id="signup">
-                <div class="col-md-12">
-                    <br />
-                    <h2 class="subtitle">Sign Up Form</h2>
-                    <h3>Sign up is now live!!! Simply download and fill out the form below and send to <a href="mailto:info@forestevasion.co.uk">info@forestevasion.co.uk</a></h3>
-                    <div class="col-md-6">
-                        <a href="files/Forest Evasion 2016 Team Sign Up Form.pdf" target="_blank">
-                            <div class="well" style="font-size:large; text-align:center;">
-                                <i class="fa fa-file-pdf-o fa-4x"></i><br />
-                                Download the form in PDF format
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a href="http://view.officeapps.live.com/op/view.aspx?src=http://forestevasion.co.uk/files/Forest Evasion 2016 Team Sign Up Form.docx" target="_blank">
-                            <div class="well" style="font-size:large; text-align:center;">
-                                <i class="fa fa-file-word-o fa-4x"></i><br />
-                                Download the form in Word format
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="row" id="details">
-                <div class="col-md-12">
-                    <h3>Rules and Kit List</h3>
-                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                        <div class="panel panel-default" style="background-color: #f5f5f5;">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseRules" aria-expanded="false" aria-controls="collapseRules">
-                                <div class="panel-heading" role="tab" id="headingRules">
-                                    <h4 class="panel-title">
-                                        <i class="fa fa-map"></i>&nbsp;Evasion Rules
-                                    </h4>
-                                </div>
-                            </a>
-                            <div id="collapseRules" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingRules">
-                                <div class="panel-body">
-                                    <ul>
-                                        <li>Teams of Evaders consist of 4-7 people</li>
-                                        <li>Competitiors must be between the ages of 18 - 35 and part of a registered organisation such as University, Scouts or Guides</li>
-                                        <li>Evaders must navigate between given checkpoints. Catchers aim to 'catch' (tag) evading teams. Evaders lose 1 life if caught. Catchers gain points depending on how many lives a caught team has. The evading team that completes the course with least lives lost will win the evaders trophy. The catching team with most points will win the Catchers Trophy. After a team is caught they will have 15 minutes free time from any catching team and 30 mins from the same catching team.</li>
-                                        <li>Teams cannot be caught within 500m of a checkpoint whilst that checkpoint is open. If an evading team does not reach a checkpoint before it closes it must contact HQ1 phone with a 6 digit grid reference.</li>
-                                        <li>Anyone who is unable to show that they have any kit on the compulsory kit list may not be allowed to enter. All entrants must read the H&S briefing.</li>
-                                        <li>You are responsible for your own safety and that of your team.</li>
-                                        <li>All clothing, including shoes, should be suited to cover a long distance.</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default" style="background-color: #f5f5f5;">
-                            <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseKit" aria-expanded="false" aria-controls="collapseKit">
-                                <div class="panel-heading" role="tab" id="headingKit">
-                                    <h4 class="panel-title">
-                                        <i class="fa fa-check-square-o"></i>&nbsp;Compulsory Kit List (not definitive)
-                                    </h4>
-                                </div>
-                            </a>
-                            <div id="collapseKit" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingKit">
-                                <div class="panel-body">
-                                    <ul>
-                                        <li>Backpack</li>
-                                        <li>Water bottle</li>
-                                        <li>Hiking boots</li>
-                                        <li>Base layers</li>
-                                        <li>Insulation</li>
-                                        <li>Outer shell (waterproof)</li>
-                                        <li>Socks</li>
-                                        <li>Spare socks</li>
-                                        <li>Hat, gloves, scarf, suncream etc.</li>
-                                        <li>Toilet paper</li>
-                                        <li>Food (you may buy additional food whilst competing, but we cannot guarantee where will be open, especially on sunday)</li>
-                                        <li>Torch</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="row" id="winners">
                 <div class="col-md-12">
                     <br />
                     <h2 class="subtitle">Previous Winners</h2>
                 </div>
             </div>
+            <div class="row padded-row-bottom">
+                <div class="col-md-12">
+                    <p>The competition first ran in 2013, the winners to date are as follows:</p>
+                    <h3>Evaders</h3>
+                    <p>2016 - Southampton City Network (Tom Candy, Peter Ogilvie, Arshad Emmambux )</p>
+                    <p>2015 - Cambridge University Hillwalking Club (Philip Withnall, Vicky Ward, Joe Hobbs, Chris Arran, Tom Leach)</p>
+                    <p>2014 - Bath University Mountaineering Club (David Clare, Jon Ridley, Strachan McCormick, Martin MacDonald)</p>
+                    <p>2013 - Southampton's Duke of Edinburgh Society (Rachel Manson, Charlotte Callaway, Alex Manning, Georgia Howes)</p>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-md-4">
-                    <img src="images/plaque2013.jpg" class="img-responsive shadow-image"/>
-                    <br />
-                    <br />
+                    <img class="img-responsive shadow-image" src="images/UEA.jpg" />
                 </div>
-                <div class="col-md-8">
-                    <p>The competition first ran in 2013, the winners to date are as follows:</p>
-                    <h3>Catchers</h3>
-                    <p>2013 - Southampton University Hillwalking Club Peter Forrest</p>
-                    <p>2014 - Southampton University Hillwalking Club Joe Warmington</p>
-                    <p>2015 - Southampton University Hillwalking Club (Tom Masheder, Tom Candy, Jake Rowland)</p>
-                    <p>2016 - ???</p>
-                    <h3>Evaders</h3>
-                    <p>2013 - Southampton's Duke of Edinburgh Society (Rachel Manson, Charlotte Callaway, Alex Manning, Georgia Howes)</p>
-                    <p>2014 - Bath University Mountaineering Club (David Clare, Jon Ridley, Strachan McCormick, Martin MacDonald)</p>
-                    <p>2015 - Cambridge University Hillwalking Club (Philip Withnall, Vicky Ward, Joe Hobbs, Chris Arran, Tom Leach)</p>
-                    <p>2016 - ???</p>
+                <div class="col-md-4">
+                    <img class="img-responsive shadow-image" src="images/Group_Wide.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img class="img-responsive shadow-image" src="images/SSAGO.jpg" />
                 </div>
             </div>
-            
-            <div class="row" id="2015">
-                <div class="col-md-12">
-                    <br />
-                    <h2 class="subtitle">2015 Infographic</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <iframe align="centre" width="100%" height="3800" frameborder="0" scrolling="no" style="overflow-y:hidden;" src="https://magic.piktochart.com/embed/5783006-forest-evasion-3-infographic"></iframe>
-                </div>
-            </div>
-            <div class="row">
+            <div class="row padded-row-top">
                 <div class="col-md-6 col-md-offset-3" style="text-align:center;">
                     <i class="fa fa-copyright"></i> Forest Evasion 2016 <a href="forestevasion.co.uk">forestevasion.co.uk</a>
                 </div>
             </div>
         </div>        
     </body>
-</html>
+    </html>
+                
