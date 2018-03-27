159cf8747979366786112885ab621cf416e56d81
Lloyd <superdyoll@gmail.com>
2018-03-27 17:08:22 -0400

    Corrected to enable https

--- includes/head.php
+++ includes/head.php
@@ -2,9 +2,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta name="description" content="Forest Evasion"/>
 <meta name="author" content=""/>
-<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 <script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/navbar.css">
 <script>
@@ -17,4 +17,4 @@
 	ga('send', 'pageview');
 	
 </script>
-<link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Ubuntu:400,500,700" rel="stylesheet" type="text/css">
